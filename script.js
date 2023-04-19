b121966bee49dd2c8938c4497d6537439996579a
airtondias <airtondias2930@gmail.com>
2023-04-18 22:00:08 -0300

    comentario

--- script.js
+++ script.js
@@ -1,15 +1,64 @@
-let array
-console.log('a. ',array)
+//let array
+//console.log('a. ',array)
 
-array = null
-console.log('b. ', array)
+//array = null
+//console.log('b. ', array)
 
-array = [3,4,5,6,7,8,9,10,11,12,13]
-console.log('c. ', array.length)
+//array = [3,4,5,6,7,8,9,10,11,12,13]
+//console.log('c. ', array.length)
 
-let i = 0
-console.log('d. ', array[i])
-array[i+1] = 19
-console.log('e. ', array)
-const valor = array [i+6]
-console.log('f. ', valor)
+//let i = 0
+//console.log('d. ', array[i])
+//array[i+1] = 19
+//console.log('e. ', array)
+//const valor = array [i+6]
+//console.log('f. ', valor)
+//const frase = prompt ("digite uma frase")
+//console.log(frase.toUpperCase().replaceAll("A", "i"),frase.length)
+
+//faca um programa que pergunte ao usuario seu nome e seu emaill. 
+//em seguida, imprima no console a seguinte mensagem 
+
+// o email foi cadastrado com susseço.
+// seja bem vindo (A) nome do usuario!
+//const nome = prompt ("digite seu nome")
+
+//const frase = prompt ("digite email")
+//console.log ('o email foi cadastrado com susseço.'+ email + 'foi cadastrado com susseço'+'seja bem -vindo,'+ nome + '!');
+// faça um programa,seguindo os passos
+//a) crie um array vazio e guarde em uma variavel, chamada lista de tarefas 
+//b) pergunte ao usuario 3 tarefas que ele precise realizar no dia e armazene - as uma por uma no array 
+//c) Imprima o array no console
+    
+    //d) Peça ao usuário que digite o *índice* de uma tarefa que ele já realizou: 0, 1 ou 2 
+    
+   // e) Remova da lista o item de índice que o usuário escolheu.
+    
+    //f) Imprima o array no console
+const listadetarefas = []
+const tarefa1 = prompt ("digite tarefa 1 ")
+const tarefa2 = prompt ("digite tarefa 2")
+const tarefa3 = prompt ("digite tarefa 3")
+
+listadetarefas = [tarefa1,tarefa2,tarefa3]
+console.log(listadetarefas)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
